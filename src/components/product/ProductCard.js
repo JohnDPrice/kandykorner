21adebe7249390f97938e4a589236db847f617e7
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-03-01 14:23:04 -0500

    Added product type to product component

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -4,5 +4,6 @@ import "./Product.css"
 export const ProductCard = ({ product }) => (
     <section className="product">
         <h3 className="product___name">{product.name}</h3>
+        <p className="product__type">Type: {product.productType.type}</p>
     </section>
 )
